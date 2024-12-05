ee394c0346cda33dca60bf4e101d4aa20efe705e
Brithany <brithannylainez@gmail.com>
2024-12-04 22:15:53 -0500

    start commit added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import express from 'express'
 import cors from 'cors' //importa los paquetes cors --permisos de accesos
 import { fileURLToPath } from 'url'
-import clientesRoutes from './routes/clientes.routes.js'
-import usuariosRoutes from './routes/usuarios.routes.js'
-import pedidosRoutes from './routes/pedidos.routes.js'
-import productosRoutes from './routes/productos.routes.js'
-import detalleRoutes from './routes/detalleP.routes.js'
+
 import path from 'path'
 
 //definir el modulo de ES
@@ -23,11 +19,6 @@ app.use(express.json()); // Para que interprete los objetos JSON
 app.use(express.urlencoded({extended:true})); //se añade para poder receptar formularios
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 // Rutas
-app.use('/api', clientesRoutes);
-app.use('/api', usuariosRoutes);
-app.use('/api', productosRoutes); 
-app.use('/api', pedidosRoutes);
-app.use('/api', detalleRoutes);
 
 
 // Middleware para rutas no encontradas
--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import { config } from 'dotenv'
 config()
 
-export const BD_HOST = process.env.BD_HOST || "btk8de7pxk98orbqfjrh-mysql.services.clever-cloud.com";
-export const BD_DATABASE = process.env.BD_DATABASE || "btk8de7pxk98orbqfjrh";
-export const DB_USER = process.env.DB_USER || "uak1u17ha3ixovhg";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "6h1co0P0prNoeEzAvnxe";
+export const BD_HOST = process.env.BD_HOST || "bomx9cb2sko8auroqocp-mysql.services.clever-cloud.com";
+export const BD_DATABASE = process.env.BD_DATABASE || "bomx9cb2sko8auroqocp";
+export const DB_USER = process.env.DB_USER || "ukotbuku24lrgszx";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "jECLS3RyHZ12xzXr4HkN";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET || "anthony123";
